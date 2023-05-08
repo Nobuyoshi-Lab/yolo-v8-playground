adfdc1367fa5ccdb8080f04c116df2c804ad3bff
Ka Ming Lui <tuk91907@temple.edu>
2023-05-07 23:49:23 -0400

    added model choosing feat.

--- yolo_v8_object_detection.py
+++ yolo_v8_object_detection.py
@@ -1,6 +1,7 @@
 import os
 import tkinter as tk
 from tkinter import filedialog, simpledialog
+from tkinter import ttk
 from ultralytics import YOLO
 import ultralytics.yolo.data.utils as utils
 
@@ -8,44 +9,36 @@ CONFIDENCE_THRESHOLD = 0.55
 IMAGE_FORMATS = utils.IMG_FORMATS
 VIDEO_FORMATS = utils.VID_FORMATS
 
+TASK_DICT = {
+    "Detection": "",
+    "Instance Segmentation": "-seg",
+    "Pose/Keypoints": "-pose",
+    "Classification": "-cls"
+}
+SIZE_DICT = {
+    "Nano": "n",
+    "Small": "s",
+    "Medium": "m",
+    "Large": "l",
+    "Extra Large": "x"
+}
+
 
 class YoloFileProcessor:
-    """
-    This class is responsible for processing files with YOLO model.
-    """
-
     def __init__(self, model_version='yolov8n-seg.pt'):
         self.yolo_model = self._load_model(model_version)
 
     @staticmethod
     def _load_model(model_version):
-        """
-        Load the YOLO model with the given model version.
-
-        :param model_version: str, the version of the YOLO model to load
-        :return: YOLO object
-        """
         current_dir = os.path.dirname(os.path.realpath(__file__))
         weights_dir = os.path.join(current_dir, 'weights')
         model_path = os.path.join(weights_dir, model_version)
         return YOLO(model_path)
 
     def process_file(self, file_path):
-        """
-        Process the given file with the YOLO model.
-
-        :param file_path: str, path of the file to be processed
-        :return: bool, True if the file was processed successfully, False otherwise
-        """
         return self._predict_with_yolo(file_path)
 
     def _predict_with_yolo(self, file_path):
-        """
-        Predict objects in the given file using the YOLO model.
-
-        :param file_path: str, path of the file to predict objects
-        :return: bool, True if the prediction was successful, False otherwise
-        """
         results_gen = self.yolo_model.predict(
             source=file_path,
             conf=CONFIDENCE_THRESHOLD,
@@ -57,113 +50,140 @@ class YoloFileProcessor:
         return results_gen is not None
 
 
-class FileSelector:
-    """
-    This class is responsible for selecting a file or URL using a GUI dialog.
-    """
+class CustomUrlDialog(simpledialog.Dialog):
+    def body(self, master):
+        ttk.Label(master, text="Enter the URL:").grid(row=0)
+        self.url_input = ttk.Entry(master, width=50)
+        self.url_input.grid(row=0, column=1)
+        return self.url_input
 
-    @staticmethod
-    def select_input():
-        """
-        Show a dialog for the user to select whether to use a file or a URL.
+    def apply(self):
+        self.result = self.url_input.get()
 
-        :return: str, 'file' or 'url' based on the user's choice
-        """
-        def set_choice(choice):
-            user_choice.set(choice)
-            root.quit()
 
-        root = tk.Tk()
-        root.title("Select Input Type")
-        root.geometry("300x100")
+class InputSelectionDialog(tk.Toplevel):
+    def __init__(self, parent=None, title=None):
+        super().__init__(parent)
+        self.title(title)
+        self.geometry("300x250")
+        self.choice = None
 
-        user_choice = tk.StringVar(value="file")
+        self.protocol("WM_DELETE_WINDOW", self.quit)
+        self.task_var = tk.StringVar(self)
+        self.size_var = tk.StringVar(self)
+        self.create_input_selection_window()
 
-        tk.Radiobutton(
-            root,
+    def set_choice(self, choice):
+        self.choice = choice
+        self.destroy()
+
+    def quit(self):
+        self.choice = None
+        self.destroy()
+
+    def create_input_selection_window(self):
+        window = ttk.Frame(self)
+        window.pack(padx=20, pady=20)
+
+        file_button = ttk.Button(
+            window,
             text="File",
-            variable=user_choice,
-            value="file",
-            command=lambda: set_choice("file"),
-            width=20,
-            height=3).pack(
-            anchor=tk.W)  # Add width and height
-        tk.Radiobutton(
-            root,
+            command=lambda: self.set_choice("file"),
+            width=20
+        )
+        file_button.pack(pady=10)
+
+        url_button = ttk.Button(
+            window,
             text="URL",
-            variable=user_choice,
-            value="url",
-            command=lambda: set_choice("url"),
-            width=20,
-            height=3).pack(
-            anchor=tk.W)
+            command=lambda: self.set_choice("url"),
+            width=20
+        )
+        url_button.pack(pady=10)
 
-        root.mainloop()
-        root.destroy()
+        self.task_var.set("Instance Segmentation")
+        self.size_var.set("Nano")
 
-        return user_choice.get()
+        task_menu = ttk.OptionMenu(
+            window,
+            self.task_var,
+            "Instance Segmentation",
+            *TASK_DICT.keys())
+        task_menu.pack(pady=10)
 
-    @staticmethod
-    def select_file():
-        """
-        Open a file selection dialog and return the selected file path.
-
-        :return: str, path of the selected file
-        """
-        root = tk.Tk()
-        root.withdraw()
-        file_path = filedialog.askopenfilename(
-            filetypes=[
-                ('Video files', f"*{';*'.join(VIDEO_FORMATS)}"),
-                ('Image files', f"*{';*'.join(IMAGE_FORMATS)}")],
-            title='Select a file')
-        root.destroy()
-        return file_path
-
-    @staticmethod
-    def ask_url():
-        """
-        Open a URL input dialog and return the entered URL.
-
-        :return: str, the entered URL
-        """
-        root = tk.Tk()
-        root.withdraw()
-        url = simpledialog.askstring("Enter URL", "Enter the URL:")
-        root.destroy()
-        return url
+        size_menu = ttk.OptionMenu(
+            window, self.size_var, "Nano", *SIZE_DICT.keys())
+        size_menu.pack(pady=10)
 
 
-def process_input():
-    """
-    Process a file or URL with the YOLO model using a file selector dialog or URL input dialog.
-    """
-    input_type = FileSelector.select_input()
+def select_input(parent):
+    input_dialog = InputSelectionDialog(parent)
+    input_dialog.wait_window()
 
-    if input_type == "file":
-        file_path = FileSelector.select_file()
-    else:
-        file_path = FileSelector.ask_url()
+    return input_dialog.choice
 
-    if not file_path:
-        print("No input provided. Exiting...")
-        return
 
-    print(f"Processing {input_type}: {file_path}")
-    processor = YoloFileProcessor()
-    is_successful = processor.process_file(file_path)
+def select_file(parent):
+    file_path = filedialog.askopenfilename(
+        parent=parent,
+        filetypes=[
+            ('Video files', f"*{';*'.join(VIDEO_FORMATS)}"),
+            ('Image files', f"*{';*'.join(IMAGE_FORMATS)}")],
+        title='Select a file')
+    return file_path
 
-    if is_successful:
-        print(f"Succesfully processed {input_type}")
-    else:
-        print(f"Failed to process {input_type}")
+
+def ask_url(parent):
+    url_dialog = CustomUrlDialog(parent=parent, title="Enter URL")
+    return url_dialog.result
+
+
+class YoloApp:
+    def __init__(self):
+        self.yolo_processor = None
+
+    def process_input(self, parent):
+        input_type = select_input(parent)
+
+        if input_type is None:
+            print("Exiting...")
+            return
+
+        if input_type == "file":
+            file_path = select_file(parent)
+        else:
+            file_path = ask_url(parent)
+
+        if not file_path:
+            print("No input provided. Exiting...")
+            return
+
+        task_suffix = parent.task_var.get()
+        size_suffix = parent.size_var.get()
+
+        model_version = f"yolov8{SIZE_DICT[size_suffix]}{TASK_DICT[task_suffix]}.pt"
+        self.yolo_processor = YoloFileProcessor(model_version=model_version)
+
+        print(f"Processing {input_type}: {file_path}")
+        is_successful = self.yolo_processor.process_file(file_path)
+
+        if is_successful:
+            print(f"Succesfully processed {input_type}")
+        else:
+            print(f"Failed to process {input_type}")
 
 
 def main():
-    """
-    The main entry point of the script.
-    """
-    process_input()
+    root = tk.Tk()
+    root.withdraw()
+
+    app = YoloApp()
+    input_choice = app.process_input(root)
+
+    if input_choice is None:
+        return
+    else:
+        root.mainloop()
 
 
 if __name__ == '__main__':
